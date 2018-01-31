4e64d9da6b9e007ed4928cabd728bc602d0212bb
Artem Nikitin <artemnikitin@artemnikitin.com>
2018-01-31 15:49:20 +0200

    Update Go implementation for run on Travis CI

--- ci/build-and-test.sh
+++ ci/build-and-test.sh
@@ -7,3 +7,13 @@ cmake ../src \
     -DCMAKE_CXX_FLAGS="-Wall -pedantic -Wextra"
 make
 make test
+
+cd /src/flatdata-go
+make setup-ci
+cd $GOPATH/src/github.com/heremaps/flatdata/flatdata-go
+make run-ci
+
+cd /src/flatdata-go-backward-compatibility
+make setup-ci
+cd $GOPATH/src/github.com/heremaps/flatdata/flatdata-go-backward-compatibility
+make run-ci
