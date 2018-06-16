a320db94a9977847de1d9e411559a056f2f95d00
Christian Vetter <veaac.fdirct@gmail.com>
2018-06-16 14:10:42 +0200

    Remove leftover c&p from cpp generator

--- generator/generators/FlatdataGenerator.py
+++ generator/generators/FlatdataGenerator.py
@@ -27,11 +27,6 @@ class FlatdataGenerator(BaseGenerator):
             return False
         env.filters["filter_builtin"] = lambda l: [x for x in l if not _is_builtin(x)]
 
-        def _safe_cpp_string_line(value):
-            return value.replace('\\', '\\\\').replace('"', r'\"')
-
-        env.filters["safe_cpp_string_line"] = _safe_cpp_string_line
-
         def _field_type(t):
             return t.replace("@@", ".").replace("@", ".")
 
@@ -42,28 +37,5 @@ class FlatdataGenerator(BaseGenerator):
 
         env.filters["to_type_params"] = _to_type_params
 
-        def _typedef_name(entity, extra_suffix=""):
-            assert isinstance(entity, Field) or isinstance(entity,
-                                                           ResourceBase), "Got: %s" % \
-                                                                          entity.__class__
-            return "".join([c.title() for c in entity.name.split('_')]) + extra_suffix + "Type"
-
-        env.filters["typedef_name"] = _typedef_name
-
-        def _optional_typedef_usage(resource, extra_suffix=""):
-            def _wrap_in_optional(declaration):
-                if resource.optional:
-                    return "boost::optional< %s >" % declaration
-                return declaration
-
-            return _wrap_in_optional(_typedef_name(resource, extra_suffix))
-
-        env.filters["archive_typedef_usage"] = _optional_typedef_usage
-
         env.filters["supported_resources"] = lambda l: [x for x in l if
                                                         not isinstance(x, BoundResource)]
-        env.filters["simple_resources"] = lambda l: [x for x in l if
-                                                     not isinstance(x, BoundResource) and
-                                                     not isinstance(x, ArchiveResource)]
-        env.filters["archive_resources"] = lambda l: [x for x in l if
-                                                      isinstance(x, ArchiveResource)]
