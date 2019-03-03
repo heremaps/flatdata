2b9916e9f99f721c51865a8e50171fcef9273311
Christian Vetter <veaac.fdirct@gmail.com>
2019-03-03 11:46:49 +0100

    Convert cpp generator tests
    Signed-off-by: Christian Vetter <veaac.fdirct@gmail.com>

---
+++ tests/generators/cpp_expectations/constants/namespaces.h
@@ -0,0 +1,13 @@
+namespace n { 
+enum : int8_t
+{
+    FOO = 0
+};
+} // namespace n
+
+namespace m { 
+enum : int8_t
+{
+    FOO = 1
+};
+} // namespace m
---
+++ tests/generators/cpp_expectations/constants/values.h
@@ -0,0 +1,279 @@
+namespace n { 
+enum : int8_t
+{
+    FOO_I8_NEG = -128
+};
+} // namespace n
+
+namespace n { 
+enum : int8_t
+{
+    FOO_I8_POS = 127
+};
+} // namespace n
+
+namespace n { 
+enum : int8_t
+{
+    FOO_I8_ZERO = 0
+};
+} // namespace n
+
+namespace n { 
+enum : int8_t
+{
+    FOO_I8_NEG_HEX = -128
+};
+} // namespace n
+
+namespace n { 
+enum : int8_t
+{
+    FOO_I8_POS_HEX = 127
+};
+} // namespace n
+
+namespace n { 
+enum : int8_t
+{
+    FOO_I8_ZERO_HEX = 0
+};
+} // namespace n
+
+namespace n { 
+enum : uint8_t
+{
+    FOO_U8_POS = 255
+};
+} // namespace n
+
+namespace n { 
+enum : uint8_t
+{
+    FOO_U8_ZERO = 0
+};
+} // namespace n
+
+namespace n { 
+enum : uint8_t
+{
+    FOO_U8_POS_HEX = 255
+};
+} // namespace n
+
+namespace n { 
+enum : uint8_t
+{
+    FOO_U8_ZERO_HEX = 0
+};
+} // namespace n
+
+namespace n { 
+enum : int16_t
+{
+    FOO_I16_NEG = -32768
+};
+} // namespace n
+
+namespace n { 
+enum : int16_t
+{
+    FOO_I16_POS = 32767
+};
+} // namespace n
+
+namespace n { 
+enum : int16_t
+{
+    FOO_I16_ZERO = 0
+};
+} // namespace n
+
+namespace n { 
+enum : int16_t
+{
+    FOO_I16_NEG_HEX = -32768
+};
+} // namespace n
+
+namespace n { 
+enum : int16_t
+{
+    FOO_I16_POS_HEX = 32767
+};
+} // namespace n
+
+namespace n { 
+enum : int16_t
+{
+    FOO_I16_ZERO_HEX = 0
+};
+} // namespace n
+
+namespace n { 
+enum : uint16_t
+{
+    FOO_U16_POS = 65535
+};
+} // namespace n
+
+namespace n { 
+enum : uint16_t
+{
+    FOO_U16_ZERO = 0
+};
+} // namespace n
+
+namespace n { 
+enum : uint16_t
+{
+    FOO_U16_POS_HEX = 65535
+};
+} // namespace n
+
+namespace n { 
+enum : uint16_t
+{
+    FOO_U16_ZERO_HEX = 0
+};
+} // namespace n
+
+namespace n { 
+enum : int32_t
+{
+    FOO_I32_NEG = -2147483648
+};
+} // namespace n
+
+namespace n { 
+enum : int32_t
+{
+    FOO_I32_POS = 2147483647
+};
+} // namespace n
+
+namespace n { 
+enum : int32_t
+{
+    FOO_I32_ZERO = 0
+};
+} // namespace n
+
+namespace n { 
+enum : int32_t
+{
+    FOO_I32_NEG_HEX = -2147483648
+};
+} // namespace n
+
+namespace n { 
+enum : int32_t
+{
+    FOO_I32_POS_HEX = 2147483647
+};
+} // namespace n
+
+namespace n { 
+enum : int32_t
+{
+    FOO_I32_ZERO_HEX = 0
+};
+} // namespace n
+
+namespace n { 
+enum : uint32_t
+{
+    FOO_U32_POS = 4294967295
+};
+} // namespace n
+
+namespace n { 
+enum : uint32_t
+{
+    FOO_U32_ZERO = 0
+};
+} // namespace n
+
+namespace n { 
+enum : uint32_t
+{
+    FOO_U32_POS_HEX = 4294967295
+};
+} // namespace n
+
+namespace n { 
+enum : uint32_t
+{
+    FOO_U32_ZERO_HEX = 0
+};
+} // namespace n
+
+namespace n { 
+enum : int64_t
+{
+    FOO_I64_NEG = -9223372036854775808
+};
+} // namespace n
+
+namespace n { 
+enum : int64_t
+{
+    FOO_I64_POS = 9223372036854775807
+};
+} // namespace n
+
+namespace n { 
+enum : int64_t
+{
+    FOO_I64_ZERO = 0
+};
+} // namespace n
+
+namespace n { 
+enum : int64_t
+{
+    FOO_I64_NEG_HEX = -9223372036854775808
+};
+} // namespace n
+
+namespace n { 
+enum : int64_t
+{
+    FOO_I64_POS_HEX = 9223372036854775807
+};
+} // namespace n
+
+namespace n { 
+enum : int64_t
+{
+    FOO_I64_ZERO_HEX = 0
+};
+} // namespace n
+
+namespace n { 
+enum : uint64_t
+{
+    FOO_U64_POS = 18446744073709551615
+};
+} // namespace n
+
+namespace n { 
+enum : uint64_t
+{
+    FOO_U64_ZERO = 0
+};
+} // namespace n
+
+namespace n { 
+enum : uint64_t
+{
+    FOO_U64_POS_HEX = 18446744073709551615
+};
+} // namespace n
+
+namespace n { 
+enum : uint64_t
+{
+    FOO_U64_ZERO_HEX = 0
+};
+} // namespace n
