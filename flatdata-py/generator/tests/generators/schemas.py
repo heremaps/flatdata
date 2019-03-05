912b2b592b1154edae04111d47e80d4d4b75e301
Christian Vetter <veaac.fdirct@gmail.com>
2019-03-05 19:15:19 +0100

    Adjust to changed folder structure
    Signed-off-by: Christian Vetter <veaac.fdirct@gmail.com>

--- flatdata-py/generator/tests/generators/schemas.py
+++ flatdata-py/generator/tests/generators/schemas.py
@@ -3,7 +3,7 @@ import os
 def schemas_and_expectations(generator, extension):
     result = list()
     basedir = os.path.dirname(__file__)
-    test_dir = os.path.normpath(os.path.join(basedir, '..', '..', 'test_cases'))
+    test_dir = os.path.normpath(os.path.join(basedir, '..', '..', '..','..', 'test_cases'))
     for path, subdirs, files in os.walk(test_dir):
         for name in files:
             if os.path.splitext(name)[1] == '.flatdata':
@@ -13,4 +13,5 @@ def schemas_and_expectations(generator, extension):
                                                generator + '_expectations',
                                                os.path.splitext(test_relpath)[0]+'.' + extension)
                 result.append((os.path.join(path, name), expecation_path))
+    assert len(result) == 23, "Did not find expected number of test cases in " + test_dir
     return result
