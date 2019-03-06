6aae529c1ed1ff1fdc138a2e55d09192c1019662
Christian Vetter <veaac.fdirct@gmail.com>
2019-03-06 08:11:38 +0100

    Start on test case generation
    Signed-off-by: Christian Vetter <veaac.fdirct@gmail.com>

---
+++ flatdata-rs/test_cases/build.rs
@@ -0,0 +1,36 @@
+use std::io::Write;
+use std::path::PathBuf;
+
+fn main() {
+    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let test_cases_path = dir.join("../../test_cases").canonicalize().unwrap();
+
+    for entry in walkdir::WalkDir::new(&test_cases_path) {
+        let entry = entry.expect("Cannot access file");
+        if entry.path().extension().map_or(true, |x| x != "flatdata") {
+            continue;
+        }
+
+        let mut result: PathBuf = entry.path().strip_prefix(&test_cases_path).unwrap().into();
+        result.set_extension("rs");
+
+        let filename = out_path.join(&result);
+        std::fs::create_dir_all(filename.parent().unwrap()).expect("Failed to create output dir");
+        let output = std::process::Command::new("python3")
+            .args(&[
+                "../../generator",
+                "-g",
+                "rust",
+                "-s",
+                entry.path().to_str().expect("Invalid path"),
+                "-O",
+                filename.to_str().expect("Invalid path"),
+            ])
+            .output()
+            .expect("Failed to run generator");
+
+        std::io::stderr().write_all(&output.stderr).unwrap();
+        assert!(output.status.success());
+    }
+}
