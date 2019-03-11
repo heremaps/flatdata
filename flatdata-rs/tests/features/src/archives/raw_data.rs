42119202637210f2feec933d870cb89a28a1bb4a
Christian Vetter <veaac.fdirct@gmail.com>
2019-03-11 22:16:27 +0100

    Add raw_data/vector tests
    Signed-off-by: Christian Vetter <veaac.fdirct@gmail.com>

--- flatdata-rs/tests/features/src/archives/raw_data.rs
+++ flatdata-rs/tests/features/src/archives/raw_data.rs
@@ -1,3 +1,31 @@
 #![allow(dead_code)]
 
 include!(concat!(env!("OUT_DIR"), "/archives/raw_data.rs"));
+
+#[test]
+fn test() {
+    use flatdata::Archive;
+    use flatdata::ArchiveBuilder;
+
+    for &set_optional in &[false, true] {
+        let storage = flatdata::MemoryResourceStorage::new("/my_test");
+
+        let builder = n::ABuilder::new(storage.clone()).expect("Failed to create builder");
+        builder.set_data(b"My Data").expect("Failed to set data");
+
+        if set_optional {
+            builder
+                .set_optional_data(b"My Other Data")
+                .expect("Failed to set optional data");
+        }
+
+        let archive = n::A::open(storage).expect("Failed to open archive");
+        assert_eq!(archive.data(), b"My Data");
+        if set_optional {
+            assert_eq!(
+                archive.optional_data().expect("Optional data missing"),
+                b"My Other Data"
+            );
+        }
+    }
+}
--- flatdata-rs/tests/features/src/archives/vector.rs
+++ flatdata-rs/tests/features/src/archives/vector.rs
@@ -1,3 +1,50 @@
 #![allow(dead_code)]
 
 include!(concat!(env!("OUT_DIR"), "/archives/vector.rs"));
+
+#[test]
+fn test() {
+    use flatdata::Archive;
+    use flatdata::ArchiveBuilder;
+
+    for &set_optional in &[false, true] {
+        let storage = flatdata::MemoryResourceStorage::new("/my_test");
+
+        let mut data = flatdata::Vector::<n::S>::new();
+        for x in 10..600 {
+            data.grow().set_x(x);
+        }
+
+        let builder = n::ABuilder::new(storage.clone()).expect("Failed to create builder");
+        builder
+            .set_data(&data.as_view())
+            .expect("Failed to set data");
+
+        if set_optional {
+            let mut optional_data = builder
+                .start_optional_data()
+                .expect("Failed to start optional data");
+            for x in 10..600 {
+                optional_data
+                    .grow()
+                    .expect("Failed to grow optional data")
+                    .set_x(x);
+            }
+            optional_data
+                .close()
+                .expect("Failed to close optional data");
+        }
+
+        let archive = n::A::open(storage).expect("Failed to open archive");
+        let data = archive.data();
+        for x in (10..600).enumerate() {
+            assert_eq!(data.at(x.0).x(), x.1);
+        }
+        if set_optional {
+            let data = archive.optional_data().expect("Optional data not found");
+            for x in (10..600).enumerate() {
+                assert_eq!(data.at(x.0).x(), x.1);
+            }
+        }
+    }
+}
