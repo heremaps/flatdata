d4fb29f62d1506de7e3291dde2f6f6d13dd4998f
Gabriel Féron <gabriel.feron@here.com>
2019-03-12 13:14:03 +0100

    Fix wrong volume mount in travis CI
    Signed-off-by: Gabriel Féron <gabriel.feron@here.com>

--- flatdata-cpp/ci/build-and-test-cpp.sh
+++ flatdata-cpp/ci/build-and-test-cpp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 
-cmake ../src \
+cmake ../src/flatdata-cpp \
     -DCMAKE_C_COMPILER=${C_COMPILER} \
     -DCMAKE_CXX_COMPILER=${CXX_COMPILER} \
     -DCMAKE_CXX_FLAGS="-Wall -pedantic -Wextra"
