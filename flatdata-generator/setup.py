ae677c0c7e5b4171ba1d658c4dccc2241b552aab
Gabriel Féron <feron.gabriel@gmail.com>
2019-03-17 16:48:26 +0100

    Provide a quick way to generate flatdata rust code with a build.rs
    Internally, it relies on a virtualenv, in which we install flatdata-generator
    from either PyPI or a source directory provided in the env (FLATDATA_GENERATOR_PATH)
    
    Also:
    * Upgrade Travis CI to Ubuntu Bionic (18.04)
    
    Signed-off-by: Gabriel Féron <feron.gabriel@gmail.com>

--- flatdata-generator/setup.py
+++ flatdata-generator/setup.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
+import os
 from setuptools import find_packages, setup
+
+SOURCE_FILEPATH = os.path.dirname(os.path.abspath(__file__))
 
 setup(
     name="flatdata-generator",
     version="0.3.0-2",
     author="Flatdata Developers",
     description="Generate source code for C++, Rust, Go or Python from a Flatdata schema file",
-    long_description=open("README.md").read(),
+    long_description=open(os.path.join(SOURCE_FILEPATH, "README.md")).read(),
     long_description_content_type="text/markdown",
     url="https://github.com/heremaps/flatdata",
     # we can't use find_namespace_packages as it is only a very recent addition to setuptools
-    packages=['flatdata.' + p for p in find_packages('flatdata')],
+    packages=["flatdata." + p for p in find_packages("flatdata")],
     package_data={
-        '': ['*.jinja2'],
+        "": ["*.jinja2"],
     },
     entry_points={
-        'console_scripts': ['flatdata-generator=flatdata.generator.app:main'],
+        "console_scripts": ["flatdata-generator=flatdata.generator.app:main"],
     },
+    setup_requires=["wheel"],
     install_requires=[
         "pyparsing>=2.0",
         "jinja2>=2.2",
--- flatdata-rs/tests/coappearances/build.rs
+++ flatdata-rs/tests/coappearances/build.rs
@@ -1,15 +1,3 @@
-use std::path::PathBuf;
-
 fn main() {
-    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    let test_cases_path = PathBuf::from("../../../examples/coappearances/coappearances.flatdata");
-    let generator_path = PathBuf::from("../../../generator");
-
-    match flatdata::generate(&out_path, &test_cases_path, &generator_path) {
-        Err(e) => {
-            eprintln!("{}", e);
-            std::process::exit(1);
-        }
-        _ => (),
-    }
+    flatdata::generate("../../../examples/coappearances/coappearances.flatdata").unwrap();
 }
