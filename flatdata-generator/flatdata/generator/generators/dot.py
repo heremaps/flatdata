f38ab420e9930411c7ad42fab87c77362cfd9506
Christian Vetter <christian.vetter@here.com>
2021-11-15 09:57:19 +0100

    Resolve enum names for Dot diagrams
    Signed-off-by: Christian Vetter <christian.vetter@here.com>

--- flatdata-generator/flatdata/generator/generators/dot.py
+++ flatdata-generator/flatdata/generator/generators/dot.py
@@ -19,5 +19,17 @@ class DotGenerator(BaseGenerator):
     def _populate_environment(self, env):
         env.autoescape = True
 
+        def _field_value_type(field):
+            type_name = field.type.name.replace("@@", ".").replace("@", ".")
+            namespace_name = field.parent.parent.path
+            if type_name.startswith(namespace_name):
+                type_name = type_name[len(namespace_name):]
+            if type_name.startswith("."):
+                type_name = type_name[1:]
+
+            return type_name
+
+        env.filters["field_value_type"] = _field_value_type
+
     def supported_nodes(self):
         return [Archive]
