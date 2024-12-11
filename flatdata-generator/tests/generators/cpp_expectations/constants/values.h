c363c329cf26a5ba7ef2b52d6a2d62695057f718
mohapatr3 <prasanta.mohapatra@here.com>
2024-12-11 14:49:11 +0530

    annotation for const and enum
    Signed-off-by: mohapatr3 <prasanta.mohapatra@here.com>

--- flatdata-generator/tests/generators/cpp_expectations/constants/values.h
+++ flatdata-generator/tests/generators/cpp_expectations/constants/values.h
@@ -141,139 +141,139 @@ enum : uint16_t
 namespace n { 
 enum : int32_t
 {
-    FOO_I32_NEG = -2147483648
+    FOO_I32_NEG = -2147483648L
 };
 } // namespace n
 
 namespace n { 
 enum : int32_t
 {
-    FOO_I32_POS = 2147483647
+    FOO_I32_POS = 2147483647L
 };
 } // namespace n
 
 namespace n { 
 enum : int32_t
 {
-    FOO_I32_ZERO = 0
+    FOO_I32_ZERO = 0L
 };
 } // namespace n
 
 namespace n { 
 enum : int32_t
 {
-    FOO_I32_NEG_HEX = -2147483648
+    FOO_I32_NEG_HEX = -2147483648L
 };
 } // namespace n
 
 namespace n { 
 enum : int32_t
 {
-    FOO_I32_POS_HEX = 2147483647
+    FOO_I32_POS_HEX = 2147483647L
 };
 } // namespace n
 
 namespace n { 
 enum : int32_t
 {
-    FOO_I32_ZERO_HEX = 0
+    FOO_I32_ZERO_HEX = 0L
 };
 } // namespace n
 
 namespace n { 
 enum : uint32_t
 {
-    FOO_U32_POS = 4294967295
+    FOO_U32_POS = 4294967295UL
 };
 } // namespace n
 
 namespace n { 
 enum : uint32_t
 {
-    FOO_U32_ZERO = 0
+    FOO_U32_ZERO = 0UL
 };
 } // namespace n
 
 namespace n { 
 enum : uint32_t
 {
-    FOO_U32_POS_HEX = 4294967295
+    FOO_U32_POS_HEX = 4294967295UL
 };
 } // namespace n
 
 namespace n { 
 enum : uint32_t
 {
-    FOO_U32_ZERO_HEX = 0
+    FOO_U32_ZERO_HEX = 0UL
 };
 } // namespace n
 
 namespace n { 
 enum : int64_t
 {
-    FOO_I64_NEG = -9223372036854775808
+    FOO_I64_NEG = -9223372036854775808LL
 };
 } // namespace n
 
 namespace n { 
 enum : int64_t
 {
-    FOO_I64_POS = 9223372036854775807
+    FOO_I64_POS = 9223372036854775807LL
 };
 } // namespace n
 
 namespace n { 
 enum : int64_t
 {
-    FOO_I64_ZERO = 0
+    FOO_I64_ZERO = 0LL
 };
 } // namespace n
 
 namespace n { 
 enum : int64_t
 {
-    FOO_I64_NEG_HEX = -9223372036854775808
+    FOO_I64_NEG_HEX = -9223372036854775808LL
 };
 } // namespace n
 
 namespace n { 
 enum : int64_t
 {
-    FOO_I64_POS_HEX = 9223372036854775807
+    FOO_I64_POS_HEX = 9223372036854775807LL
 };
 } // namespace n
 
 namespace n { 
 enum : int64_t
 {
-    FOO_I64_ZERO_HEX = 0
+    FOO_I64_ZERO_HEX = 0LL
 };
 } // namespace n
 
 namespace n { 
 enum : uint64_t
 {
-    FOO_U64_POS = 18446744073709551615
+    FOO_U64_POS = 18446744073709551615ULL
 };
 } // namespace n
 
 namespace n { 
 enum : uint64_t
 {
-    FOO_U64_ZERO = 0
+    FOO_U64_ZERO = 0ULL
 };
 } // namespace n
 
 namespace n { 
 enum : uint64_t
 {
-    FOO_U64_POS_HEX = 18446744073709551615
+    FOO_U64_POS_HEX = 18446744073709551615ULL
 };
 } // namespace n
 
 namespace n { 
 enum : uint64_t
 {
-    FOO_U64_ZERO_HEX = 0
+    FOO_U64_ZERO_HEX = 0ULL
 };
 } // namespace n
